4cb99320b84d0da54e7ffb07fb5e049cc4996d19
Nemadur <piekos.jan@gmail.com>
2022-09-20 22:54:07 +0200

    first concrete commit

--- js/core.js
+++ js/core.js
@@ -3,4 +3,27 @@ $(document).ready(core_init);
 window.semantic_init = core_init;
 
 function core_init() {
+
+    $('.climbButton').on('click', progress);
+    $('#resetButton').on('click', reset);
+
+
+
+    $('#bar').progress({
+      label: 'ratio',
+      text: {
+        ratio: '{value} / {total}'
+      }
+    })
+  ;
+
 };
+
+function progress(btn){
+    let value = +btn.target.value;
+    $('#bar').progress('increment', value);
+}
+
+function reset(btn){
+    $('#bar').progress(reset);
+}
